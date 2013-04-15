e31750aca66269472101f967f3f72729f39187d7
Byron Hulcher <byron@strayboots.com>
2013-04-15 17:26:31 -0400

    Forced application/json Content-Type to include a charset, as application expects.  Revert "Fixed a bug where JSON/JSONP was returning an invalid ``charset`` within the MIME type. Thanks to dstufft for the report!"
    This reverts commit 609f2e29b3b0055dc1081f2fc138643e34716fb3.
    
    Conflicts:
    	tastypie/utils/mime.py
    	tests/core/tests/utils.py

--- tests/core/tests/utils.py
+++ tests/core/tests/utils.py
@@ -8,22 +8,18 @@ from tastypie.utils.mime import determine_format, build_content_type
 
 class MimeTestCase(TestCase):
     def test_build_content_type(self):
-        # JSON & JSONP don't include charset.
-        self.assertEqual(build_content_type('application/json'), 'application/json')
-        self.assertEqual(build_content_type('text/javascript'), 'text/javascript')
-        self.assertEqual(build_content_type('application/json', encoding='ascii'), 'application/json')
-        # Everything else should.
+        self.assertEqual(build_content_type('application/json'), 'application/json; charset=utf-8')
         self.assertEqual(build_content_type('application/xml'), 'application/xml; charset=utf-8')
-        self.assertEqual(build_content_type('application/xml', encoding='ascii'), 'application/xml; charset=ascii')
-
+        self.assertEqual(build_content_type('application/json; charset=ascii'), 'application/json; charset=ascii')
+    
     def test_determine_format(self):
         serializer = Serializer()
         full_serializer = Serializer(formats=['json', 'jsonp', 'xml', 'yaml', 'html', 'plist'])
         request = HttpRequest()
-
+        
         # Default.
         self.assertEqual(determine_format(request, serializer), 'application/json')
-
+        
         # Test forcing the ``format`` parameter.
         request.GET = {'format': 'json'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
@@ -38,19 +34,20 @@ class MimeTestCase(TestCase):
 
         request.GET = {'format': 'xml'}
         self.assertEqual(determine_format(request, serializer), 'application/xml')
-
+        
         request.GET = {'format': 'yaml'}
         self.assertEqual(determine_format(request, serializer), 'text/yaml')
-
+        
         request.GET = {'format': 'plist'}
         self.assertEqual(determine_format(request, serializer), 'application/x-plist')
-
+        
         request.GET = {'format': 'foo'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
-
+        
         # Test the ``Accept`` header.
         request.META = {'HTTP_ACCEPT': 'application/json'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
+<<<<<<< HEAD
 
         # Again, disabled by default.
         request.META = {'HTTP_ACCEPT': 'text/javascript'}
@@ -60,30 +57,36 @@ class MimeTestCase(TestCase):
         request.META = {'HTTP_ACCEPT': 'text/javascript'}
         self.assertEqual(determine_format(request, full_serializer), 'text/javascript')
 
+=======
+        
+        request.META = {'HTTP_ACCEPT': 'text/javascript'}
+        self.assertEqual(determine_format(request, serializer), 'text/javascript')
+        
+>>>>>>> parent of 609f2e2... Fixed a bug where JSON/JSONP was returning an invalid ``charset`` within the MIME type. Thanks to dstufft for the report!
         request.META = {'HTTP_ACCEPT': 'application/xml'}
         self.assertEqual(determine_format(request, serializer), 'application/xml')
-
+        
         request.META = {'HTTP_ACCEPT': 'text/yaml'}
         self.assertEqual(determine_format(request, serializer), 'text/yaml')
-
+        
         request.META = {'HTTP_ACCEPT': 'application/x-plist'}
         self.assertEqual(determine_format(request, serializer), 'application/x-plist')
-
+        
         request.META = {'HTTP_ACCEPT': 'text/html'}
         self.assertEqual(determine_format(request, serializer), 'text/html')
-
+        
         request.META = {'HTTP_ACCEPT': '*/*'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
-
+        
         request.META = {'HTTP_ACCEPT': 'application/json,application/xml;q=0.9,*/*;q=0.8'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
-
+        
         request.META = {'HTTP_ACCEPT': 'text/plain,application/xml,application/json;q=0.9,*/*;q=0.8'}
         self.assertEqual(determine_format(request, serializer), 'application/xml')
-
+        
         request.META = {'HTTP_ACCEPT': 'application/json; charset=UTF-8'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
-
+        
         request.META = {'HTTP_ACCEPT': 'text/javascript,application/json'}
         self.assertEqual(determine_format(request, serializer), 'application/json')
 
